1dd1bfe2e341ff9dc11a715a995f9fbe11fbb435
jadilovic <adilovic79@yahoo.com>
2022-05-25 23:46:04 +0200

    fixed for heroku

--- server/index.js
+++ server/index.js
@@ -4,11 +4,21 @@ const app = express();
 
 const mongoDB = require('./db/connect');
 const projectRoute = require('./routes/project');
+// added for heroku
+const path = require('path');
 
 app.use(express.json());
 app.use(cors());
 
+//added for heroku
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.use('/api/v1/project', projectRoute);
+
+// added for heroku
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 8080;
 
